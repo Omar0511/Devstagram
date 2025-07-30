e52d9c4eb00662a285e9ec422e8b57a511aed0bf
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-07-29 21:04:02 -0600

    Funcion DD, como leer datos ingresados

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -11,7 +11,14 @@ class RegisterController extends Controller
         return view("auth.register");
     }
 
-    public function store() {
-        dd('Post...');
+    public function store(Request $request) {
+        // dd: imprime lo que le pases, pero detiene la ejecución TOTAL
+        // dd('Post...');
+
+        // Debugueando
+        // dd($request);
+
+        // De esta forma validamos los valores de los INPUT apuntando al NAME
+        dd($request->get('username') );
     }
 }
