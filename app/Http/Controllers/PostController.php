51bb4b4f6dd9789f8a51ee84a8d8703901916688
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-08-21 23:38:40 -0600

    Creando el MURO

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -6,10 +6,17 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    //Proteger rutas
+    public function __construct() {
+        // Middleware: Protege las rutas, para que solo los usuarios autenticados puedan acceder
+        $this->middleware('auth');
+    }
+
     public function index() {
         // dd('Desde muro');
         // Revisar que usuario esta autenticado
-        dd( auth()->user() );
+        // dd( auth()->user() );
+
+        return view('dashboard');
     }
 }
