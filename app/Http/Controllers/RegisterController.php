afb2ef9e6bad8c8efe02335160f5c4304291f6b2
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-08-21 22:01:33 -0600

    Otra forma de autenticar

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -56,11 +56,16 @@ class RegisterController extends Controller
         );
 
         // Autenticar al usuario
+        // auth()->attempt(
+        //     [
+        //         'email' => $request->email,
+        //         'password' => $request->password
+        //     ]
+        // );
+
+        // Otra forma de autenticar al usuario
         auth()->attempt(
-            [
-                'email' => $request->email,
-                'password' => $request->password
-            ]
+            $request->only('email', 'password')
         );
 
         // Redireccionamos al usuario
