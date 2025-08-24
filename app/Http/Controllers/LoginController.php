6171914a570591f634202b433c4f973b8cb615df
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-08-23 23:30:26 -0600

    Agregando libreria para validacion de FORMULARIO

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Iluminate\Foundation
+use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Support\Str;
 
 class LoginController extends Controller
 {
+    use ValidatesRequests; // 👈 ESTE use es el que aplica el trait, lo tenemos que poner cuando usamos: VALIDATE
     //
     public function index() {
         return view("auth.login");
     }
 
     public function store(Request $request) {
-        //
         // dd('Autenticando...');
         $this->validate($request, [
             'email' => 'required|email',
