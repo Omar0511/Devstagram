3ce222be2e3df38948d1ffc725fe83e43eaeca90
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-09-11 19:50:58 -0600

    Para que sirven los CONTRAINED en las MIGRACIONES

--- database/migrations/2025_09_11_210827_create_comentarios_table.php
+++ database/migrations/2025_09_11_210827_create_comentarios_table.php
@@ -13,8 +13,9 @@ return new class extends Migration
     {
         Schema::create('comentarios', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
-            $table->foreignId('post_id')->constrained();
+            // Con el CASCADE, si se elimina un usuario o un post, se eliminan sus comentarios
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('post_id')->constrained()->onDelete('cascade');
             $table->string('comentario');
             $table->timestamps();
         });
