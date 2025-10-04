38d9dfba8241d5ccfd0d21ff15315c1cf325ab09
Omar Garcia <za19012225@zapopan.tecmm.edu.mx>
2025-10-04 08:49:24 -0600

    Que son los  en los COMPONENTES

--- resources/views/components/listar-post.blade.php
+++ resources/views/components/listar-post.blade.php
@@ -1,3 +1,9 @@
 <div>
-    <h1>Mostrando posts</h1>
+    {{-- <h1>Mostrando posts</h1> --}}
+
+    {{-- Aquí imprimimos lo que esta dentro del SLOT, definido como HEADER o TITULO --}}
+    {{-- {{ $titulo }} --}}
+
+    {{-- Al agregar SLOT, se pasará lo que tengamos por ejemplo en la vista, ej: home.blade.php --}}
+    {{-- <h1>{{ $slot }}</h1> --}}
 </div>
